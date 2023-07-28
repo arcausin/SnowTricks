e3c259d772f1dbc0282cc2db98ac727c0f6333ac
arcausin <arcausin@gmail.com>
2023-07-28 10:31:52 +0200

    utilisation d'une autrte méthode de fetch de données pour les trikcs en homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,19 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Entity\Trick;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\TrickRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(ManagerRegistry $mr): Response
+    public function index(TrickRepository $trickRepository): Response
     {
-        $tricks = $mr->getRepository(Trick::class)->findAll();
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'tricks' => $tricks
+            'tricks' => $trickRepository->findAll(),
         ]);
     }
 }
