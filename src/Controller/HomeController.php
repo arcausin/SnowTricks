16deef49c0c3269071df47455716c82cd491e48f
arcausin <arcausin@gmail.com>
2023-07-28 10:18:11 +0200

    findAll Trick Homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Trick;
+use Doctrine\Persistence\ManagerRegistry;
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ManagerRegistry $mr): Response
     {
+        $tricks = $mr->getRepository(Trick::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'tricks' => $tricks
         ]);
     }
 }
