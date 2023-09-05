36027fb49bf14d57fdcf1c6ea71eb7a5d167ff15
arcausin <arcausin@gmail.com>
2023-09-05 04:42:43 +0200

    edit profile (username, icon, new password)

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,59 +7,75 @@ use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    #[Route('/', name: 'app_profile_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_profile_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_home');
         }
 
-        return $this->renderForm('profile/new.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);
-    }
+        $user = $this->getUser();
 
-    #[Route('/{id}', name: 'app_profile_show', methods: ['GET'])]
-    public function show(User $user): Response
-    {
-        return $this->render('profile/show.html.twig', [
-            'user' => $user,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->get('plainPassword')->getData() == '') {
+                $user->setPassword($user->getPassword());
+            } else {
+                // encode the plain password
+                $user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
+            }
+
+            $illustrationFile = $form->get('illustration')->getData();
+
+            if ($illustrationFile) {
+                // Delete old illustration file if it exists
+                $this->deleteIllustrationFile($user);
+
+                $originalFilename = pathinfo($illustrationFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$illustrationFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $illustrationFile->move(
+                        $this->getParameter('illustrations_profile_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $user->setIllustration($newFilename);
+            }
+
+            if ($form->get('username')->getData() == '') {
+                $user->setUsername($user->getUsername());
+            } else {
+                $user->setUsername($form->get('username')->getData());
+            }
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('profile/edit.html.twig', [
@@ -68,14 +84,14 @@ class ProfileController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_profile_delete', methods: ['POST'])]
-    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    private function deleteIllustrationFile(User $user): void
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($user);
-            $entityManager->flush();
+        $oldFilename = $user->getIllustration();
+        if ($oldFilename) {
+            $filePath = $this->getParameter('illustrations_profile_directory') . '/' . $oldFilename;
+            if (file_exists($filePath)) {
+                unlink($filePath);
+            }
         }
-
-        return $this->redirectToRoute('app_profile_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,21 +4,57 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('isVerified')
-            ->add('username')
-            ->add('illustration')
-        ;
+            ->add('username', TextType::class, [
+                'label' => 'Username',
+                'required' => true,
+            ])
+            ->add('illustration', FileType::class, [
+                'label' => 'Illustration',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp',
+                            'image/gif',
+                            'image/tiff',
+                            'image/bmp',
+                        ],
+                    ])
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'label' => 'New password',
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                ],
+                'invalid_message' => 'The password fields must match.',
+                'mapped' => false,
+                'required' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
