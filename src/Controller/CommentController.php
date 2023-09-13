e1d361984ed0040c833dc77c74c33a951f066599
arcausin <arcausin@gmail.com>
2023-09-13 08:50:40 +0200

    correction error 500 delete with id url

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -15,17 +15,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     #[Route('/{id}/delete', name: 'app_comment_delete', methods: ['POST'])]
-    public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, EntityManagerInterface $entityManager, CommentRepository $commentRepository, int $id): Response
     {
         if (!$this->getUser() || $this->getUser() !== $comment->getUser()) {
             return $this->redirectToRoute('app_home');
         }
-        
+
+        $comment = $commentRepository->find($id);
+
         if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
             $entityManager->remove($comment);
             $entityManager->flush();
         }
-        
+
         $targetUrl = $this->generateUrl('app_trick_show', ['slug' => $comment->getTrick()->getSlug()]) . '#commentSection';
         return $this->redirect($targetUrl);
     }
--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -15,11 +15,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class MediaController extends AbstractController
 {
     #[Route('/{id}/delete', name: 'app_media_delete', methods: ['POST'])]
-    public function delete(Request $request, Media $media, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, EntityManagerInterface $entityManager, MediaRepository $mediaRepository, int $id): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_home');
         }
+
+        $media = $mediaRepository->find($id);
 
         // Delete illustration file if it exists
         $this->deleteIllustrationFile($media);
